08a52abcdcd278efa35fa9d48c784cba680e940a
Anniee <52150815+Anni1123@users.noreply.github.com>
2020-10-18 23:36:51 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 import pyrebase
 config={
-    "apiKey": "AIzaSyA1_TbZc_DAJVAosBsBXHKVnANss0_220U",
-    "authDomain": "freshers-portal.firebaseapp.com",
-    "databaseURL": "https://freshers-portal.firebaseio.com",
-    "projectId": "freshers-portal",
-    "storageBucket": "freshers-portal.appspot.com",
-    "messagingSenderId": "620782197376",
-    "appId": "1:620782197376:web:f7835cc81df3aced7d2465",
-    "measurementId": "G-KH43ST917G"
+    "apiKey": "#Add Api ",
+    "authDomain": "#Add Here",
+    "databaseURL": "#Add Here",
+    "projectId": "#Add Here",
+    "storageBucket": "#Add Here",
+    "messagingSenderId": "#Add Here",
+    "appId": "#Add Here",
+    "measurementId": "#Add Here"
 };
 
 

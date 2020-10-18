21a6d9b4dfb528abbdc212c662e816c15c9ff077
Anni1123 <kumarianni1213@gmail.com>
2020-10-18 12:48:26 +0530

    Added Basic Configuation of firebase

--- main/views.py
+++ main/views.py
@@ -1,6 +1,22 @@
 # viewsfile
 from django.shortcuts import render
 
+import pyrebase
+config={
+    "apiKey": "AIzaSyA1_TbZc_DAJVAosBsBXHKVnANss0_220U",
+    "authDomain": "freshers-portal.firebaseapp.com",
+    "databaseURL": "https://freshers-portal.firebaseio.com",
+    "projectId": "freshers-portal",
+    "storageBucket": "freshers-portal.appspot.com",
+    "messagingSenderId": "620782197376",
+    "appId": "1:620782197376:web:f7835cc81df3aced7d2465",
+    "measurementId": "G-KH43ST917G"
+};
+
+
+firebase=pyrebase.initialize_app(config)
+
+
 def HomePage(request):
     return render(request,"Home.html")
 

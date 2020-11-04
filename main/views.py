109d416a45b52642798b9793b694a3d1d456ca3e
Mohd Zubair <55323065+Mohdzubair3056@users.noreply.github.com>
2020-11-05 00:10:49 +0530

    add reset link

--- main/views.py
+++ main/views.py
@@ -1,10 +1,7 @@
 # viewsfile
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 import pyrebase
-
 config={
     "apiKey": "AIzaSyA1_TbZc_DAJVAosBsBXHKVnANss0_220U",
     "authDomain": "freshers-portal.firebaseapp.com",
@@ -16,8 +13,10 @@ config={
     "measurementId": "G-KH43ST917G",
 }
 firebase=pyrebase.initialize_app(config)
-authe = firebase.auth()
 database=firebase.database()
+authe = firebase.auth()
+
+
 
 def HomePage(request):
     import datetime
@@ -29,7 +28,6 @@ def HomePage(request):
     Titles = []
     Types = []
     Departments = []
-    images = []
     Writtenbys = []
     for i in lis_time:
         Department = database.child('Blogs').child(i).child('Department').get().val()
@@ -42,78 +40,45 @@ def HomePage(request):
         Titles.append(Title)
         Types.append(Type)
         name = database.child('users').child(Writtenby).child('name').get().val()
-        image = database.child('users').child(Writtenby).child('imgUrl').get().val()
-        print(image)
         Writtenbys.append(name)
-        images.append(image)
     date = []
     for i in timestamp:
         i = float(i)
         dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%y')
         date.append(dat)
-
-    comb_lis = zip(lis_time, date, Descriptions,Departments,Titles,Types,Writtenbys,images)
+    comb_lis = zip(lis_time, date, Descriptions,Departments,Titles,Types,Writtenbys)
     return render(request,"Home.html",{"comb_lis":comb_lis})
 
-@csrf_exempt
-def search(request):
-    return render(request,"Search.html")
 def signIn(request):
     return render(request,"Login.html")
 
 def postsignIn(request):
-    if request.method=='POST':
+    email = request.POST.get('email')
+    pasw = request.POST.get('pass')
+    try:
+        user = authe.sign_in_with_email_and_password(email, pasw)
+    except:
+        message = "Invalid Credentials!!Please Check your Data"
+        return render(request, "Login.html", {"message": message})
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+    return render(request, "ProfilePage.html", {"email": email})
+
+def resetPass(request):
+    return render(request, "Reset.html")
+
+def postReset(request):
+    try:
         email = request.POST.get('email')
-        pasw = request.POST.get('pass')
-        try:
-            user = authe.sign_in_with_email_and_password(email, pasw)
-        except:
-            message = "Invalid Credentials!!Please ChecK your Data"
-            return render(request, "Login.html", {"message": message})
-        session_id = user['idToken']
-        request.session['uid'] = str(session_id)
-        idToken = request.session['uid']
-        if idToken:
-            a = authe.get_account_info(idToken)
-            a = a['users']
-            a = a[0]
-            uid = a['localId']
-            import datetime
-            timestamp = database.child('Blogs').shallow().get().val()
-            lis_time = []
-            for i in timestamp:
-                lis_time.append(i)
-            Descriptions = []
-            Titles = []
-            Types = []
-            Departments = []
-            Writtenbys = []
-            for i in lis_time:
-                Department = database.child('Blogs').child(i).child('Department').get().val()
-                Description = database.child('Blogs').child(i).child('Description').get().val()
-                Title = database.child('Blogs').child(i).child('Title').get().val()
-                Type = database.child('Blogs').child(i).child('Type').get().val()
-                Writtenby = database.child('Blogs').child(i).child('Writtenby').get().val()
-                if uid == Writtenby:
-                    Departments.append(Department)
-                    Descriptions.append(Description)
-                    Titles.append(Title)
-                    Types.append(Type)
-                    name = database.child('users').child(Writtenby).child('name').get().val()
-                    Writtenbys.append(name)
-            date = []
-            for i in timestamp:
-                i = float(i)
-                dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%y')
-                date.append(dat)
-            name = database.child('users').child(uid).child('name').get().val()
-            branch = database.child('users').child(uid).child('branch').get().val()
-            image = database.child('users').child(uid).child('imgUrl').get().val()
-            print(image)
-            comb_lis = zip(lis_time, date, Descriptions, Departments, Titles, Types, Writtenbys)
-            return render(request, "ProfilePage.html", {"comb_lis": comb_lis, "name": name, "branch": branch,"image":image})
-    message = "Please Login In First"
-    return render(request, "Login.html", {"message": message})
+        authe.send_password_reset_email(email)
+        message = "A reset link is succesfully sent to your email. Please check your email to reset password."
+        return render(request,"Reset.html",{"msg":message})
+    except:
+        message = "Please enter correct email, email you entered is not registered yet."
+        return render(request, "Reset.html",{"msg":message})
+    
+    
+
 def logout(request):
     try:
         del request.session['uid']
@@ -125,174 +90,87 @@ def signUp(request):
     return render(request,"Registration.html")
 
 def postsignup(request):
-    if request.method == 'POST':
-        name=request.POST.get('name')
-        branch=request.POST.get('sel')
-        enroll=request.POST.get('enrolls')
-        roll=request.POST.get('roll')
-        email=request.POST.get('email')
-        passw=request.POST.get('pass')
-        try:
-            user=authe.create_user_with_email_and_password(email,passw)
-        except:
-            messg="unable to create account try again"
-            return render(request,"registration.html",{"messg":messg})
-        uid = user['localId']
-        data={"name":name,"USER_TYPE":"user","device_token":"","email":email,"id":roll,"imgUrl":"","branch":branch,"uid":uid,"enrollment":enroll}
-        database.child("users").child(uid).set(data)
-        return render(request,"login.html")
-    message = "Please Login In First"
-    return render(request, "Login.html", {"message": message})
+    name=request.POST.get('name')
+    branch=request.POST.get('sel')
+    enroll=request.POST.get('enrolls')
+    roll=request.POST.get('roll')
+    email=request.POST.get('email')
+    passw=request.POST.get('pass')
+    try:
+        user=authe.create_user_with_email_and_password(email,passw)
+        authe.send_email_verification(user['idToken'])
+    except:
+        messg="unable to create account try again"
+        return render(request,"registration.html",{"messg":messg})
+    uid = user['localId']
+    data={"name":name,"USER_TYPE":"user","device_token":"","email":email,"id":roll,"imgUrl":"","branch":branch,"uid":uid,"enrollment":enroll}
+    database.child("users").child(uid).set(data)
+    return render(request,"login.html",{"message":"A verification link is send to your mail id please confirm it to signin to your account."})
+
+@require_http_methods(["GET", "POST"])
 def profile(request):
+    return render(request,"ProfilePage.html")
+
+def addPost(request):
+    return render(request,"AddPost.html")
+
+def afteraAddPost(request):
+    from datetime import datetime, timezone
+    import time
+    import pytz
     idToken = request.session['uid']
     if idToken:
+        tz = pytz.timezone('Asia/Kolkata')
+        Currenttime = datetime.now(timezone.utc).astimezone(tz).strftime("%H%M%S")
+        millis = int(Currenttime)
+        tyype = request.POST.get('type')
+        title = request.POST.get('title')
+        description = request.POST.get('desc')
+        branch=request.POST.get('sel')
         a = authe.get_account_info(idToken)
         a = a['users']
         a = a[0]
-        uid = a['localId']
-        import datetime
-        timestamp = database.child('Blogs').shallow().get().val()
-        lis_time = []
-        for i in timestamp:
-            lis_time.append(i)
-        Descriptions = []
-        Titles = []
-        Types = []
-        Departments = []
-        Writtenbys = []
-        for i in lis_time:
-            Department = database.child('Blogs').child(i).child('Department').get().val()
-            Description = database.child('Blogs').child(i).child('Description').get().val()
-            Title = database.child('Blogs').child(i).child('Title').get().val()
-            Type = database.child('Blogs').child(i).child('Type').get().val()
-            Writtenby = database.child('Blogs').child(i).child('Writtenby').get().val()
+        a = a['localId']
+        print(str(a))
 
-            if uid == Writtenby:
-                Departments.append(Department)
-                Descriptions.append(Description)
-                Titles.append(Title)
-                Types.append(Type)
-                name = database.child('users').child(Writtenby).child('name').get().val()
-                branch = database.child('users').child(Writtenby).child('branch').get().val()
-                Writtenbys.append(name)
-        date = []
-        for i in timestamp:
-            i = float(i)
-            dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%y')
-            date.append(dat)
-        name = database.child('users').child(uid).child('name').get().val()
-        branch = database.child('users').child(uid).child('branch').get().val()
-        image = database.child('users').child(uid).child('imgUrl').get().val()
-        comb_lis = zip(lis_time, date, Descriptions, Departments, Titles, Types, Writtenbys)
-        return render(request,"ProfilePage.html",{"comb_lis":comb_lis,"name":name,"branch":branch,"image":image})
-
-def addPost(request):
-        return render(request,"AddPost.html")
-
-
-def afteraAddPost(request):
-    if request.method=='POST':
-        from datetime import datetime, timezone
-        import time
-        import pytz
-        idToken = request.session['uid']
-        if idToken:
-            tz = pytz.timezone('Asia/Kolkata')
-            Currenttime = datetime.now(timezone.utc).astimezone(tz).strftime("%H%M%S")
-            millis = int(Currenttime)
-            tyype = request.POST.get('type')
-            title = request.POST.get('title')
-            description = request.POST.get('desc')
-            branch=request.POST.get('sel')
-            a = authe.get_account_info(idToken)
-            a = a['users']
-            a = a[0]
-            a = a['localId']
-
-            data ={
-                "Type":tyype,
-                "Title":title,
-                "Description":description,
-                "Writtenby":a,
-                "Time":Currenttime,
-                "Department":branch,
-            }
-            database.child('Blogs').child(millis).set(data)
-            import datetime
-            a = authe.get_account_info(idToken)
-            a = a['users']
-            a = a[0]
-            a = a['localId']
-            timestamp = database.child('Blogs').shallow().get().val()
-            lis_time = []
-            for i in timestamp:
-                lis_time.append(i)
-            Descriptions = []
-            Titles = []
-            Types = []
-            Departments = []
-            Writtenbys = []
-            for i in lis_time:
-                Department = database.child('Blogs').child(i).child('Department').get().val()
-                Description = database.child('Blogs').child(i).child('Description').get().val()
-                Title = database.child('Blogs').child(i).child('Title').get().val()
-                Type = database.child('Blogs').child(i).child('Type').get().val()
-                Writtenby = database.child('Blogs').child(i).child('Writtenby').get().val()
-                if a == Writtenby:
-                    Departments.append(Department)
-                    Descriptions.append(Description)
-                    Titles.append(Title)
-                    Types.append(Type)
-                    name = database.child('users').child(Writtenby).child('name').get().val()
-                    branch = database.child('users').child(Writtenby).child('branch').get().val()
-                    Writtenbys.append(name)
-            date = []
-            for i in timestamp:
-                i = float(i)
-                dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%y')
-                date.append(dat)
-            comb_lis = zip(lis_time, date, Descriptions, Departments, Titles, Types, Writtenbys)
-            return render(request, "ProfilePage.html", {"comb_lis": comb_lis,"name":name,"branch": branch})
-    message = "Please Login In First"
-    return render(request, "Login.html", {"message": message})
-
-
-
-def gotoedit(request):
-        return render(request,'editprofile.html')
-
-def postedit(request):
-    if request.method=='POST':
-        import time
-        from datetime import datetime,timezone
-        import pytz
-        tz = pytz.timezone('Asia/Kolkata')
-        time_now = datetime.now(timezone.utc).astimezone(tz)
-        millis = int(time.mktime(time_now.timetuple()))
-
-        dname=request.POST.get('dname')
-        email=request.POST.get('email')
-        course=request.POST['course']
-        branch=request.POST['branch']
-        year=request.POST['year']
-
-
-
-        idtoken=request.session['uid']
-        a = authe.get_account_info(idtoken)
-        a=a['users']
-        a=a[0]
-        a=a['localId']
-
-        data={
-            "name":dname,
-            "email":email,
-            "course":course,
-            "branch":branch,
-            "year":year,
+        data ={
+            "Type":tyype,
+            "Title":title,
+            "Description":description,
+            "Written by":a,
+            "Time":Currenttime,
+            "Department":branch,
         }
-        database.child('users').child(a).update(data)
 
-        return render(request,'editprofile.html')
-    message = "Please Login In First"
-    return render(request, "Login.html", {"message": message})
+        database.child('Blogs').child(millis).set(data)
+        return render(request,"ProfilePage.html")
+
+def search(request):
+    value = request.POST.get('search')
+    data = database.child('users').shallow().get().val()
+    uidlist = []
+    requid='null'
+    for i in data:
+        uidlist.append(i)
+    for i in uidlist:
+        val = database.child('users').child(i).child('name').get().val()
+        if(val == value):
+            requid = i
+    print(requid)
+
+    name = database.child('users').child(requid).child('name').get().val()
+    course = database.child('users').child(requid).child('course').get().val()
+    branch = database.child('users').child(requid).child('branch').get().val()
+    img = database.child('users').child(requid).child('imgUrl').get().val()
+    Name=[]
+    Name.append(name)
+    Course=[]
+    Course.append(course)
+    Branch=[]
+    Branch.append(branch)
+    Image=[]
+    Image.append(img)
+    comb_lis = zip(Name,Course,Branch,Image)
+    return render(request,"Search.html",{"comb_lis":comb_lis})
+
+
